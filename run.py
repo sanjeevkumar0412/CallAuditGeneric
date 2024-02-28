2a21c5f12e6c01ce8da3c2086f256928195d6ec2
Sudhir.Kumar <sudhir.kumar@agreeya.net>
2024-02-28 15:00:46 +0530

    deleted commented code

--- run.py
+++ run.py
@@ -1,23 +1,11 @@
-import os
-from dotenv import load_dotenv
+
 from app.model.start_transcribe import StartTranscribe
-from loguru import logger
-
-load_dotenv()
 
 
-def main():
-    # Your main program logic goes here
+def main():   
     try:
-        transcribe_model = StartTranscribe()      
-      #   source_file_path ="D:/Cogent_Audio_Repo/"
-      #   destination_folder = "D:/Cogent_AI_Audio_Repo/"
-      #   is_source_path_exist = os.path.exists(source_file_path)
-      #   is_destination_path_exist = os.path.exists(destination_folder) 
-      #   if is_source_path_exist and is_destination_path_exist:
-        transcribe_model.start_transcribe_process() #Premium, Normal, Small
-      #   else:
-      #     logger.error('folder path does not exist')
+        transcribe_model = StartTranscribe() 
+        transcribe_model.start_transcribe_process()       
     except Exception as e:   
         print(f'caught {type(e)}: e',e)
 
