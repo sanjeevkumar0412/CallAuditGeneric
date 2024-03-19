be4ed092831d2c86f67aee3821b54d05479c48e4
Sudhir.Kumar <sudhir.kumar@agreeya.net>
2024-03-19 10:11:17 +0530

    add the api document file in the solution

--- flasktest.py
+++ flasktest.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, jsonify, json
+from flask_swagger_ui import get_swaggerui_blueprint
 from alchemy_encoder import AlchemyEncoder
 from db_layer.models import Client, Configurations, Logs, FileTypesInfo, Subscriptions, AudioTranscribeTracker, \
     AudioTranscribe, ClientMaster, AuthTokenManagement
@@ -8,6 +9,28 @@ app = Flask(__name__)
 from database_query_utils import *
 from database_query_utils import DBRecord
 from flack_service import FlaskDBService
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+# API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+API_URL = '/static/api_document.json'
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 db_instance = DBRecord()
 flask_service = FlaskDBService()
