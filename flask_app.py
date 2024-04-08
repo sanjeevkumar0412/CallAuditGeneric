bdc7f4daf83a316c7cb97d0034bebb3ef84279b9
sanjeevkumar0412 <sanjeev.kumar@agreeya.com>
2024-04-09 00:21:39 +0530

    Added Error handling for get sentiment table

--- flask_app.py
+++ flask_app.py
@@ -82,9 +82,7 @@ def get_transcribe_sentiment():
     client_id = int(request.args.get('clientid'))
     audio_file_name = request.args.get('audio_file')
     data = sentiment_instance.get_data_from_transcribe_table(server_name, database_name, client_id,audio_file_name)
-    if data == None:
-        data = {"Error": "Invalid table/Data not available for this " + audio_file_name}
-    return {'data': data}
+    return data
 
 
 @app.route('/get_client_master_configurations', methods=['GET'])
@@ -182,9 +180,7 @@ def get_sentiment_data():
     client_id = int(request.args.get('clientid'))
     audio_file_name = request.args.get('audio_file')
     data = sentiment_instance.get_sentiment_data_from_table(server_name, database_name, client_id,audio_file_name)
-    if data == None:
-        data = {"Error": f"File not exit {audio_file_name}"}
-    return {'data': data}
+    return data
 
 
 @app.route('/get_all_configurations', methods=['GET'])
