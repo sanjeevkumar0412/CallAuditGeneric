a88503edc6fad52263898fab29224cadf41e88f3
sanjeevkumar0412 <sanjeev.kumar@agreeya.com>
2024-05-01 15:54:01 +0530

    Modified prompt and sentimental analysis

--- static/common.js
+++ static/common.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             // $('#sentimentTable').show();
             // Make AJAX call to retrieve sentiment data
             $.ajax({
-                url: '/get_data_from_sentiment_table',
+                url: 'http://flm-vm-cogaidev:4091/get_data_from_sentiment_table',
                 type: 'GET',
                 dataType: 'json',
                 data: {
@@ -20,40 +20,75 @@ $(document).ready(function() {
                 },
                 success: function(data) {
                     // Hide loader
+					console.log(2222222222,data);
                     $('.loader').hide();
                     $('.form-group').hide();
                     $('#submitButton').hide();
                     $('#sentimentTable').show();
                     $('#backsentiment').show();
                     // Remove ClientId and Id keys from the response
+
                     delete data.ClientId;
+                    delete data.Created;
+                    delete data.Modified;
+                    delete data.SentimentStatus;
+                    delete data.SentimentScore;
                     delete data.Id;
                     // Populate table body with data
                     var sentimentData = data;
-					console.log(">>>>",Object.keys(sentimentData).length);
                     var tableBody = $('#sentimentData');
                     // Append AudioFileName first
-                    var row = $('<tr class="audio_cls"></tr>');
-                    row.append($('<td></td>').text('AudioFileName'));
-                    row.append($('<td></td>').text(sentimentData.AudioFileName));
-					$('.audio_cls').show();
-                    tableBody.empty().append(row);
+				if (sentimentData.AudioFileName !== '') {
+						var row = $('<tr class="audio_cls"></tr>');
+						row.append($('<td></td>').text('AudioFileName'));
+						row.append($('<td></td>').text(sentimentData.AudioFileName));
+						//$('.audio_cls').show();
+						tableBody.empty().append(row);
+					}
+				if (sentimentData.ActionItems !== '') {
                     // Append ActionItems next
                     row = $('<tr class="audio_cls"></tr>');
                     row.append($('<td></td>').text('ActionItems'));
                     row.append($('<td></td>').text(sentimentData.ActionItems));
                     tableBody.append(row);
                     // Append other data in the original order
-					console.log(sentimentData);
-                    $.each(sentimentData, function(key, value) {
-                        if (key !== 'AudioFileName' && key !== 'ActionItems') {
-                            var row = $('<tr></tr>');
-                            row.append($('<td></td>').text(key));
-                            row.append($('<td></td>').text(value));
-                            tableBody.append(row);
-                        }
-                    });
-					if(sentimentData.ActionItems === undefined) {
+					/*if data.status != 200 {
+                      //AudioFileName,ActionItems,Owners,Sentiment,FoulLanguage,SummaryReport,Topics,Reminder
+						$('.audio_cls').hide();
+					}*/
+				}
+				row = $('<tr></tr>');
+                    row.append($('<td></td>').text('Owners'));
+                    row.append($('<td></td>').text(sentimentData.Owners));
+                    tableBody.append(row);
+                row = $('<tr></tr>');
+                row.append($('<td></td>').text('Sentiment'));
+                row.append($('<td></td>').text(sentimentData.Sentiment));
+                tableBody.append(row);
+
+                row = $('<tr></tr>');
+                    row.append($('<td></td>').text('FoulLanguage'));
+                    row.append($('<td></td>').text(sentimentData.FoulLanguage));
+                    tableBody.append(row);
+
+
+
+                row = $('<tr></tr>');
+                    row.append($('<td></td>').text('SummaryReport'));
+                    row.append($('<td></td>').text(sentimentData.SummaryReport));
+                    tableBody.append(row);
+
+
+                row = $('<tr></tr>');
+                    row.append($('<td></td>').text('Topics'));
+                    row.append($('<td></td>').text(sentimentData.Topics));
+                    tableBody.append(row);
+
+                     row = $('<tr></tr>');
+                    row.append($('<td></td>').text('Reminder'));
+                    row.append($('<td></td>').text(sentimentData.Reminder));
+                    tableBody.append(row);
+				if(sentimentData.ActionItems === undefined) {
 					console.log('If Condition');
 							$('.audio_cls').hide();
 							//$('.audio_cls1').hide();
@@ -64,7 +99,6 @@ $(document).ready(function() {
 				}
                 },
                 error: function(xhr, status, error) {
-					$('#sentimentTable').find('thead').hide();
                     $('.loader').hide();
                     $('#sentimentTable').hide();
                     console.error('Error fetching sentiment data:', audioFileName);
